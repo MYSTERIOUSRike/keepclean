70b794d704fa2d1c3cf9713e91e02c11b64bba0a
MYSTERIOUSRike <68586168+MYSTERIOUSRike@users.noreply.github.com>
2020-08-16 22:30:40 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,22 +1,18 @@
 class paper {
     constructor(x, y, radius) {
-      
-        var options={
+          var options={
           isStatic:false,
           restitution:0.3,
           friction:0.5,
           density:1.2 
-           
-         }
+                    }
       
       this.body = Bodies.circle(x, y, radius, options);
-      //this.width = width;
-      
-      //this.height = height;
       this.radius=radius
       this.image = loadImage("paper.png");
      
       World.add(world, this.body);
+      
     }
     display(){
       var pos =this.body.position;
